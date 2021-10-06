d3786f40b3b591138e822e8249c1c0b2837406b7
Jothikrishna <56432134+JOTHIKRISHNA0109@users.noreply.github.com>
2021-10-06 23:39:46 +0530

    Create SimpleIntrest.java

---
+++ SimpleIntrest.java
@@ -0,0 +1,20 @@
+//Java Program to determine the simple intrest.
+import java.util.Scanner;
+public class Main
+{
+  
+	public static void main(String[] args) {
+    
+	  Scanner input= new Scanner(System.in);
+	  System.out.print("Principal (In Rupees: ");
+	  int Principal = input.nextInt();
+		System.out.print("Rate (In %) : ");
+		double Rate = input.nextDouble();
+		System.out.print("Time (In years): ");
+		int Time = input.nextInt();
+		double si=(Principal*Rate*Time)/100;
+		System.out.println("Simple Intrest: "+si);
+    
+	}
+  
+}
